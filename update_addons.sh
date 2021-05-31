7ec4829de423216a63575fe8d972ee346d138f80
pscaught <pscaught@gmail.com>
2021-05-31 11:48:24 -0700

    Windows config

---
+++ update_addons.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+WINDOWS_HOME_PATH='/mnt/c/Users/scott'
+
+cd "${WINDOWS_HOME_PATH}/update_addons"
+source addon_env/bin/activate
+./update_addons_sylpc.py
+deactivate
+
+echo "Updating ElvUI"
+
+cd "${WINDOWS_HOME_PATH}/elvui"
+git checkout main >/dev/null
+git reset --hard origin/main
+git pull --rebase
+
+echo "Updating AddOnSkins"
+
+cd "${WINDOWS_HOME_PATH}/addonskins"
+git checkout main >/dev/null
+git reset --hard origin/main
+git pull --rebase
+git submodule update
+
+sleep 1
