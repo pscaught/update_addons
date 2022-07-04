3df7a7e7a45cf7f2276588088bb13a0b03991b7d
pscaught <pscaught@gmail.com>
2022-07-04 00:15:27 -0700

    Removing unnecessary function

--- update_addons.py
+++ update_addons.py
@@ -148,10 +148,6 @@ class Curse():
         else:
             return
 
-def split(list_a, chunk_size):
-    for i in range(0, len(list_a), chunk_size):
-        yield list_a[i:i + chunk_size]
-
 
 def check_addon(a):
     c = Curse(a)
